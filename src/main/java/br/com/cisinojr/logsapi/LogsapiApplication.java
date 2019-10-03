a272257d45184cc1fa2b48e2b0bd54cc6e7c22d2
Cisino Junior <cisinojr@gmail.com>
2019-10-03 20:14:10 -0300

    Definicao da arquitetura

---
+++ src/main/java/br/com/cisinojr/logsapi/LogsapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cisinojr.logsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsapiApplication.class, args);
+	}
+
+}

b21059342f4f9c4c344ea9ec25b7ef287b46bcd8
Cisino Junior <cisinojr@gmail.com>
2019-10-09 00:56:29 -0300

    Refatora configuracao de cors e formato de data

--- src/main/java/br/com/cisinojr/logsapi/config/SwaggerConfig.java
+++ src/main/java/br/com/cisinojr/logsapi/config/SwaggerConfig.java
@@ -2,9 +2,6 @@ package br.com.cisinojr.logsapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -23,13 +20,4 @@ public class SwaggerConfig {
                 .build();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE", "OPTIONS");
-            }
-        };
-    }
 }
